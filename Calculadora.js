4a8e5bb31a720eb5762cc58415b50d6a408a4b10
coca <cocacore7@gmail.com>
2022-08-12 22:28:54 -0600

    Se agrego endpoint resta
    Endpoint 3000 se agrego resta de dos números

--- Calculadora.js
+++ Calculadora.js
@@ -13,6 +13,13 @@ app.post('/suma', function (req, res) {
     res.send('respuesta: ' + (Num1+Num2));
 });
 
+app.post('/resta', function (req, res) {
+	var Num1 = req.body.numero1 || 0;
+	var Num2 = req.body.numero2 || 0;
+
+    res.send('respuesta: ' + (Num1-Num2));
+});
+
 app.listen(3000, () => {
     console.log('Funcionando en puerto 3000');
 });
